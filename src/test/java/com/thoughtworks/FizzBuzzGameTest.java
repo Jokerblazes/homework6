5a6c0d0d1c85eb6a7b78e50c7ecc8e5818915444
Jokerblazes <zcd123@qq.com>
2018-04-13 16:50:39 +0800

    添加测试是第1、2、3的倍数同时不包含第1个数

--- src/test/java/com/thoughtworks/FizzBuzzGameTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzGameTest.java
@@ -96,6 +96,26 @@ public class FizzBuzzGameTest {
         fizzBuzzGame.start(70);
         assertThat(fizzBuzzGame.getResults(), equalTo(expectedResults));
     }
+
+    //是3、5、7的倍数同时不包含3的最小的数是105
+    @Test
+    public void should_return_FizzBuzzWhizz_when_is_105_multiple() {
+        List<String> expectedResults = Arrays.asList(
+                "1", "2", "Fizz", "4", "Buzz", "Fizz", "Whizz", "8", "Fizz", "Buzz",
+                "11", "Fizz", "Fizz", "Whizz", "FizzBuzz", "16", "17", "Fizz", "19", "Buzz",
+                "FizzWhizz", "22", "Fizz", "Fizz", "Buzz", "26", "Fizz", "Whizz", "29", "Fizz",
+                "Fizz", "Fizz", "Fizz", "Fizz", "Fizz", "Fizz", "Fizz", "Fizz", "Fizz", "Buzz",
+                "41", "FizzWhizz", "Fizz", "44", "FizzBuzz", "46", "47", "Fizz", "Whizz", "Buzz",
+                "Fizz", "52", "Fizz", "Fizz", "Buzz", "Whizz", "Fizz", "58", "59", "FizzBuzz",
+                "61", "62", "Fizz", "64", "Buzz", "Fizz", "67", "68", "Fizz", "BuzzWhizz",
+                "71", "Fizz", "Fizz", "74", "FizzBuzz", "76", "Whizz", "Fizz", "79", "Buzz",
+                "Fizz", "82", "Fizz", "FizzWhizz", "Buzz", "86", "Fizz", "88", "89", "FizzBuzz",
+                "Whizz", "92", "Fizz", "94", "Buzz", "Fizz", "97", "Whizz", "Fizz", "Buzz",
+                "101","Fizz","Fizz","104","FizzBuzzWhizz"
+        );
+        fizzBuzzGame.start(105);
+        assertThat(fizzBuzzGame.getResults(), equalTo(expectedResults));
+    }
     
 
 }
