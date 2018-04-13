7fc4512708b60b3536a263b4a7e6224c30a25ff4
Jokerblazes <zcd123@qq.com>
2018-04-13 16:43:55 +0800

    添加测试包含第一个数字则输出Fizz

--- src/test/java/com/thoughtworks/FizzBuzzGameTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzGameTest.java
@@ -1,10 +1,30 @@
 package com.thoughtworks;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+import static org.junit.Assert.assertThat;
+
 public class FizzBuzzGameTest {
+    private FizzBuzzGame fizzBuzzGame;
+    @Before
+    public void setUp() {
+        fizzBuzzGame = new FizzBuzzGame();
+    }
+
+    //包含数字3
+    //13 中包含3
     @Test
     public void should_return_Fizz_when_have_3() {
-
+        List<String> expectedResults = Arrays.asList(
+                "1", "2", "Fizz", "4", "Buzz", "Fizz", "Whizz", "8", "Fizz", "Buzz",
+                "11", "Fizz", "Fizz"
+        );
+        fizzBuzzGame.start(13);
+        assertThat(fizzBuzzGame.getResults(), equalTo(expectedResults));
     }
 }
