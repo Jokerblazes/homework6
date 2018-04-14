4bebbb0065181f904a2502132fccc789ce8f59a7
Wu Junhong <jhwu@thoughtworks.com>
2018-04-14 14:29:20 +0800

    refactor

--- src/main/java/com/thoughtworks/FizzBuzzGameV2.java
+++ src/main/java/com/thoughtworks/FizzBuzzGameV2.java
@@ -14,36 +14,43 @@ public class FizzBuzzGameV2 {
     private int num2 = 5;
     private int num3 = 7;
 
+    public List<String> getResults() {
+        return results;
+    }
+    
     public void start(int count) {
+        String result;
         for (int i = 1; i <= count ; i++) {
+            result = "";
             if (isContainNumber1(i)) {
-                results.add(getFizzIfContainNumber1OrNumber(num1));
+                result = getFizzIfContainNumber1(i);
             } else {
-                if (isMultipleOfNumber1(i) && !isMultipleOfNumber2(i) && !isMultipleOfNumber3(i)) {
-                    results.add(getFizzIfMultipleOfNumber1OrNumber(i));
-                } else if (!isMultipleOfNumber1(i) && isMultipleOfNumber2(i) && !isMultipleOfNumber3(i)) {
-                    results.add(getBuzzIfMultipleOfNumber2OrNumber(i));
-                } else if (!isMultipleOfNumber1(i) && !isMultipleOfNumber2(i) && isMultipleOfNumber3(i)) {
-                    results.add(getWhizzIfMultipleOfNumber3OrNumber(i));
-                } else if (isMultipleOfNumber1(i) && isMultipleOfNumber2(i) && !isMultipleOfNumber3(i)) {
-                    results.add(getFizzBuzzOrNumber(i));
-                } else if (isMultipleOfNumber1(i) && !isMultipleOfNumber2(i) && isMultipleOfNumber3(i)) {
-                    results.add(getFizzWhizzOrNumber(i));
-                } else if (!isMultipleOfNumber1(i) && isMultipleOfNumber2(i) && isMultipleOfNumber3(i)) {
-                    results.add(getBuzzWhizzOrNumber(i));
-                } else if (isMultipleOfNumber1(i) && isMultipleOfNumber2(i) && isMultipleOfNumber3(i)) {
-                    results.add(getFizzBuzzWhizzOrNumber(i));
-                } else {
-                    results.add(String.valueOf(i));
-                }
+                result += getFizzIfMultipleOfNumber1(i);
+                result += getBuzzIfMultipleOfNumber2(i);
+                result += getWhizzIfMultipleOfNumber3(i);
             }
+            if(result.equals("")){
+                result = String.valueOf(i);
+            }
+            results.add(result);
         }
     }
 
+    public String getFizzIfMultipleOfNumber1(int num){
+        return isMultipleOfNumber1(num) ? "Fizz" : "";
+    }
+
+    public String getBuzzIfMultipleOfNumber2(int num){
+        return isMultipleOfNumber2(num) ? "Buzz" : "";
+    }
+
+    public String getWhizzIfMultipleOfNumber3(int num){
+        return isMultipleOfNumber3(num) ? "Whizz" : "";
+    }
 
 
-    public String getFizzIfContainNumber1OrNumber(int num) {
-        return isContainNumber1(num) ? "Fizz" : String.valueOf(num);
+    public String getFizzIfContainNumber1(int num) {
+        return isContainNumber1(num) ? "Fizz" : "";
     }
 
    private boolean isContainNumber1(int num) {
@@ -51,21 +58,6 @@ public class FizzBuzzGameV2 {
            return num == num1;
        return num % 10 == num1 || isContainNumber1(num / 10);
    }
-
-
-
-    public List<String> getResults() {
-        return results;
-    }
-
-
-    public String getFizzIfMultipleOfNumber1OrNumber(int num) {
-        return isMultipleOfNumber1(num) ? "Fizz" : String.valueOf(num);
-    }
-
-    public String getBuzzIfMultipleOfNumber2OrNumber(int num) {
-        return isMultipleOfNumber2(num) ? "Buzz" : String.valueOf(num);
-    }
 
     private boolean isMultipleOfNumber1(int num) {
         return num % num1 == 0;
@@ -75,33 +67,45 @@ public class FizzBuzzGameV2 {
         return num % num2 == 0;
     }
 
-    public String getWhizzIfMultipleOfNumber3OrNumber(int num) {
-        return isMultipleOfNumber3(num) ? "Whizz" : String.valueOf(num);
-    }
-
     private boolean isMultipleOfNumber3(int num) {
         return num % num3 == 0;
     }
 
+//    public String getFizzIfMultipleOfNumber1OrNumber(int num) {
+//        return isMultipleOfNumber1(num) ? "Fizz" : String.valueOf(num);
+//    }
 
-    public String getFizzBuzzOrNumber(int num) {
-        if (isMultipleOfNumber1(num) && isMultipleOfNumber2(num))
-            return "FizzBuzz";
-        return String.valueOf(num);
-    }
+//    public String getBuzzIfMultipleOfNumber2OrNumber(int num) {
+//        return isMultipleOfNumber2(num) ? "Buzz" : String.valueOf(num);
+//    }
 
-    public String getFizzWhizzOrNumber(int num) {
-        return (isMultipleOfNumber1(num) && isMultipleOfNumber3(num))
-                ? "FizzWhizz" : String.valueOf(num);
-    }
+    
 
-    public String getBuzzWhizzOrNumber(int num) {
-        return (isMultipleOfNumber2(num) && isMultipleOfNumber3(num))
-                ? "BuzzWhizz" : String.valueOf(num);
-    }
+//    public String getWhizzIfMultipleOfNumber3OrNumber(int num) {
+//        return isMultipleOfNumber3(num) ? "Whizz" : String.valueOf(num);
+//    }
 
-    public String getFizzBuzzWhizzOrNumber(int num) {
-        return (isMultipleOfNumber1(num) && isMultipleOfNumber2(num) && isMultipleOfNumber3(num))
-                ? "FizzBuzzWhizz" : String.valueOf(num);
-    }
+    
+
+
+//    public String getFizzBuzzOrNumber(int num) {
+//        if (isMultipleOfNumber1(num) && isMultipleOfNumber2(num))
+//            return "FizzBuzz";
+//        return String.valueOf(num);
+//    }
+//
+//    public String getFizzWhizzOrNumber(int num) {
+//        return (isMultipleOfNumber1(num) && isMultipleOfNumber3(num))
+//                ? "FizzWhizz" : String.valueOf(num);
+//    }
+//
+//    public String getBuzzWhizzOrNumber(int num) {
+//        return (isMultipleOfNumber2(num) && isMultipleOfNumber3(num))
+//                ? "BuzzWhizz" : String.valueOf(num);
+//    }
+//
+//    public String getFizzBuzzWhizzOrNumber(int num) {
+//        return (isMultipleOfNumber1(num) && isMultipleOfNumber2(num) && isMultipleOfNumber3(num))
+//                ? "FizzBuzzWhizz" : String.valueOf(num);
+//    }
 }
--- src/test/java/com/thoughtworks/FizzBuzzGameTestV2.java
+++ src/test/java/com/thoughtworks/FizzBuzzGameTestV2.java
@@ -4,10 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 
-import java.util.Arrays;
-import java.util.List;
-
-import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
@@ -27,65 +24,97 @@ public class FizzBuzzGameTestV2 {
     //13 中包含3
     @Test
     public void should_return_Fizz_when_have_3() {
-        assertEquals(fizzBuzzGame.getFizzIfContainNumber1OrNumber(6),"6");
-        assertEquals(fizzBuzzGame.getFizzIfContainNumber1OrNumber(13), "Fizz");
+        assertEquals(fizzBuzzGame.getFizzIfContainNumber1(13), "Fizz");
     }
 
 
+    @Test
+    public void should_return_empty_string_when_not_have_3() {
+        assertEquals(fizzBuzzGame.getFizzIfContainNumber1(4), "");
+    }
     //3的倍数
+//    @Test
+//    public void should_return_Fizz_when_is_3_multiple() {
+//        assertEquals(fizzBuzzGame.getFizzIfMultipleOfNumber1OrNumber(3),"Fizz");
+//        assertEquals(fizzBuzzGame.getFizzIfMultipleOfNumber1OrNumber(5),"5");
+//    }
+//
+//    //5的倍数
+//    @Test
+//    public void should_return_Buzz_when_is_5_multiple() {
+//        assertEquals(fizzBuzzGame.getBuzzIfMultipleOfNumber2OrNumber(5),"Buzz");
+//        assertEquals(fizzBuzzGame.getBuzzIfMultipleOfNumber2OrNumber(10),"Buzz");
+//        assertEquals(fizzBuzzGame.getBuzzIfMultipleOfNumber2OrNumber(4),"4");
+//    }
+//
+//    //7的倍数
+//    @Test
+//    public void should_return_Whizz_when_is_7_multiple() {
+//        assertEquals(fizzBuzzGame.getWhizzIfMultipleOfNumber3OrNumber(7),"Whizz");
+//        assertEquals(fizzBuzzGame.getWhizzIfMultipleOfNumber3OrNumber(14),"Whizz");
+//        assertEquals(fizzBuzzGame.getWhizzIfMultipleOfNumber3OrNumber(16),"16");
+//    }
+//
+//    //是3和5的倍数同时不包含3的最小的数是15
+//    @Test
+//    public void should_return_FizzBuzz_when_is_15_multiple() {
+//        assertEquals(fizzBuzzGame.getFizzBuzzOrNumber(3),"3");
+//        assertEquals(fizzBuzzGame.getFizzBuzzOrNumber(5),"5");
+//        assertEquals(fizzBuzzGame.getFizzBuzzOrNumber(15),"FizzBuzz");
+//    }
+//
+//    //是3和7的倍数同时不包含3的最小的数是21
+//    @Test
+//    public void should_return_FizzWhizz_when_is_21_multiple() {
+//        assertEquals(fizzBuzzGame.getFizzWhizzOrNumber(3),"3");
+//        assertEquals(fizzBuzzGame.getFizzBuzzOrNumber(7),"7");
+//        assertEquals(fizzBuzzGame.getFizzBuzzOrNumber(21),"21");
+//    }
+//
+//    //是5和7的倍数同时不包含3的最小的数是70
+//    @Test
+//    public void should_return_BuzzWhizz_when_is_35_multiple() {
+//        assertEquals(fizzBuzzGame.getBuzzWhizzOrNumber(5),"5");
+//        assertEquals(fizzBuzzGame.getBuzzWhizzOrNumber(7),"7");
+//        assertEquals(fizzBuzzGame.getBuzzWhizzOrNumber(35),"BuzzWhizz");
+//    }
+//
+//    //是3、5、7的倍数同时不包含3的最小的数是105
+//    @Test
+//    public void should_return_FizzBuzzWhizz_when_is_105_multiple() {
+//        assertEquals(fizzBuzzGame.getFizzBuzzWhizzOrNumber(3),"3");
+//        assertEquals(fizzBuzzGame.getFizzBuzzWhizzOrNumber(5),"5");
+//        assertEquals(fizzBuzzGame.getFizzBuzzWhizzOrNumber(7),"7");
+//        assertEquals(fizzBuzzGame.getFizzBuzzWhizzOrNumber(105),"FizzBuzzWhizz");
+//    }
+    
     @Test
-    public void should_return_Fizz_when_is_3_multiple() {
-        assertEquals(fizzBuzzGame.getFizzIfMultipleOfNumber1OrNumber(3),"Fizz");
-        assertEquals(fizzBuzzGame.getFizzIfMultipleOfNumber1OrNumber(5),"5");
+    public void should_return_Fizz_when_multiple_of_3(){
+        assertThat(fizzBuzzGame.getFizzIfMultipleOfNumber1(6), is("Fizz"));
     }
 
-    //5的倍数
     @Test
-    public void should_return_Buzz_when_is_5_multiple() {
-        assertEquals(fizzBuzzGame.getBuzzIfMultipleOfNumber2OrNumber(5),"Buzz");
-        assertEquals(fizzBuzzGame.getBuzzIfMultipleOfNumber2OrNumber(10),"Buzz");
-        assertEquals(fizzBuzzGame.getBuzzIfMultipleOfNumber2OrNumber(4),"4");
+    public void should_return_empty_string_when_not_multiple_of_3(){
+        assertThat(fizzBuzzGame.getFizzIfMultipleOfNumber1(7), is(""));
     }
 
-    //7的倍数
     @Test
-    public void should_return_Whizz_when_is_7_multiple() {
-        assertEquals(fizzBuzzGame.getWhizzIfMultipleOfNumber3OrNumber(7),"Whizz");
-        assertEquals(fizzBuzzGame.getWhizzIfMultipleOfNumber3OrNumber(14),"Whizz");
-        assertEquals(fizzBuzzGame.getWhizzIfMultipleOfNumber3OrNumber(16),"16");
+    public void should_return_Buzz_when_multiple_of_5(){
+        assertThat(fizzBuzzGame.getBuzzIfMultipleOfNumber2(10), is("Buzz"));
     }
 
-    //是3和5的倍数同时不包含3的最小的数是15
     @Test
-    public void should_return_FizzBuzz_when_is_15_multiple() {
-        assertEquals(fizzBuzzGame.getFizzBuzzOrNumber(3),"3");
-        assertEquals(fizzBuzzGame.getFizzBuzzOrNumber(5),"5");
-        assertEquals(fizzBuzzGame.getFizzBuzzOrNumber(15),"FizzBuzz");
+    public void should_return_empty_string_when_not_multiple_of_5(){
+        assertThat(fizzBuzzGame.getBuzzIfMultipleOfNumber2(7), is(""));
+    }
+    
+    @Test
+    public void should_return_Whizz_when_multiple_of_7(){
+        assertThat(fizzBuzzGame.getWhizzIfMultipleOfNumber3(14), is("Whizz"));
     }
 
-    //是3和7的倍数同时不包含3的最小的数是21
     @Test
-    public void should_return_FizzWhizz_when_is_21_multiple() {
-        assertEquals(fizzBuzzGame.getFizzWhizzOrNumber(3),"3");
-        assertEquals(fizzBuzzGame.getFizzBuzzOrNumber(7),"7");
-        assertEquals(fizzBuzzGame.getFizzBuzzOrNumber(21),"21");
+    public void should_return_empty_string_when_not_multiple_of_7(){
+        assertThat(fizzBuzzGame.getWhizzIfMultipleOfNumber3(20), is(""));
     }
-
-    //是5和7的倍数同时不包含3的最小的数是70
-    @Test
-    public void should_return_BuzzWhizz_when_is_35_multiple() {
-        assertEquals(fizzBuzzGame.getBuzzWhizzOrNumber(5),"5");
-        assertEquals(fizzBuzzGame.getBuzzWhizzOrNumber(7),"7");
-        assertEquals(fizzBuzzGame.getBuzzWhizzOrNumber(35),"BuzzWhizz");
-    }
-
-    //是3、5、7的倍数同时不包含3的最小的数是105
-    @Test
-    public void should_return_FizzBuzzWhizz_when_is_105_multiple() {
-        assertEquals(fizzBuzzGame.getFizzBuzzWhizzOrNumber(3),"3");
-        assertEquals(fizzBuzzGame.getFizzBuzzWhizzOrNumber(5),"5");
-        assertEquals(fizzBuzzGame.getFizzBuzzWhizzOrNumber(7),"7");
-        assertEquals(fizzBuzzGame.getFizzBuzzWhizzOrNumber(105),"FizzBuzzWhizz");
-    }
-
 }
